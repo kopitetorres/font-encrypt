664c942b877dee2dadee8328c5a95c87f9349aaf
chenling <chenling2@cmiot.chinamobile.com>
2020-11-16 14:59:26 +0800

    init workspace

---
+++ src/main/java/com/github/kopitetorres/font/encrypt/FontEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.github.kopitetorres.font.encrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FontEncryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FontEncryptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kopitetorres/font/encrypt/FontEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kopitetorres.font.encrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FontEncryptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
